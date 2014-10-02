fef67249036573ca6fcd160f7a854ade2fbc23fd
Edward Z. Yang <ezyang@cs.stanford.edu>
2014-10-02 11:08:14 -0700

    Synchronize setup.js with solver.js
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

--- setup.js
+++ setup.js
@@ -1,10 +1,14 @@
+var _ = require('underscore');
 
 var Expr = require('./lib/expr');
 var Decl = require('./lib/decl');
 var Pat = require('./lib/pat');
 var Type = require('./lib/type');
- require('./lib/solver');
 
+var Ctx    = require('./ctx').Ctx;
+var MutCtx = require('./ctx').MutCtx;
+
+require('./lib/solver');
 
 Type.Arrow(Type.Var("a"), Type.Var("a")).pretty()
 Decl.parse('add (x, y) = x + y').pretty();
